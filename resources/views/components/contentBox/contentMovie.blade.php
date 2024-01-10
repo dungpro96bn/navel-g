f2b4edc536503ae4123fa9c26f287a7ae81bd98d
Dũng Bùi <dungpro96bn@gmail.com>
2024-01-10 14:22:52 +0700

    add link video on movie page

--- resources/views/components/contentBox/contentMovie.blade.php
+++ resources/views/components/contentBox/contentMovie.blade.php
@@ -8,7 +8,7 @@
         </div>
         <div id="movieList">
             <div class="movieItem" data-aos="fade-up">
-                <a href="#" data-lity="data-lity">
+                <a target="_blank" href="https://d1ekkmgtajtxvf.cloudfront.net/public_iframe_html/EHfkLxIlZJkhZSu4oGu2q1yUomEKEQuVF2jgFTERYIA5rJEPJRuOnRceFx1QZKMRFx5XAwWxA3MZqSp3GIAnGHqenII0pRW4qSD0ZQNjEREJrSq5DHH1pJEzGJ5ZLayHLKS5HGWBAKMEnGSgE29AHxueEyMODGN4DF4hYTu0qUOmBv8iMQSyqJIbqzWkMTZkowxhL2kiqJEzpz9hqP5hMKDiZQNkY2I2nKW5Y2cmY2I2nKW5YaOfLKyypv5gnJ4hnaZfYQNfZGpjAQt1AQp5Zt%3D%3D/index.html" data-lity="data-lity">
                     <p class="movieImg">
                         <picture>
                             <source srcset="{{asset('images/contentBox/movie/top_movie_img01_pc.png')}}">
@@ -19,7 +19,7 @@
                 </a>
             </div>
             <div class="movieItem" data-aos="fade-up">
-                <a href="#" data-lity="data-lity">
+                <a target="_blank" href="https://d1ekkmgtajtxvf.cloudfront.net/public_iframe_html/EHfkDzWaZ2csIwEUEJZ3ESH5p0gDHUyiIycSGaOTBGI0D0t2F1EZMJSVFSpjBJAMoRkZEUN4MQETE0EHIRgsAJb1ASyhnQZjZmEFoKOUoIuYJKquFTEbDmD5oS9iLyVlZaV0D3O4ZUEjMy82rR1yn00goKH5I2MRql4hYTu0qUOmBv8iMQSyqJIbqzWkMTZkowxhL2kiqJEzpz9hqP5hMKDiZQNkY2I2nKW5Y2cmY2I2nKW5YaOfLKyypv5gnJ4hnaZfYQNfZGpjAQt1AGZkZN%3D%3D/index.html" data-lity="data-lity">
                     <p class="movieImg">
                         <picture>
                             <source srcset="{{asset('images/contentBox/movie/top_movie_img02_pc.png')}}">
@@ -30,7 +30,7 @@
                 </a>
             </div>
             <div class="movieItem" data-aos="fade-up">
-                <a href="#" data-lity="data-lity">
+                <a target="_blank" href="https://d1ekkmgtajtxvf.cloudfront.net/public_iframe_html/EHfkpzy6q3WEn3R0EHqvZUqFoSEarIc2E2qcMKW4qScnp3y1nQEFFH9nJUyFM2DkAwSyHHg3Ayx4p25QnT03L3D2nKAsDyceBQWHZJkYG3WREJx3LKH5qQMlLGAFIzSZIR1FqyMxK21JqGR5G0SQnRcnH3DlMI9WMl4hYTu0qUOmBv8iMQSyqJIbqzWkMTZkowxhL2kiqJEzpz9hqP5hMKDiZQNkY2I2nKW5Y2cmY2I2nKW5YaOfLKyypv5gnJ4hnaZfYQNfZGpjAQt1AGR4ZN%3D%3D/index.html" data-lity="data-lity">
                     <p class="movieImg">
                         <picture>
                             <source srcset="{{asset('images/contentBox/movie/top_movie_img03_pc.png')}}">
